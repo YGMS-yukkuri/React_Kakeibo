66b4c995ae3a44a468e66779635a6fa188b8f839
yukkuri <reityan.sakura0104@gmail.com>
2025-11-04 18:23:28 +0900

    Appコンポーネントのスタイルを更新し、不要なコードを削除

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,25 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className='AppContainer'>
+        <header className='AppHeader'>
+          <h1>家計簿アプリ</h1>
+        </header>
+        <div className='InOutContainer'>
+          <div>
+            <h2>収入タブ</h2>
+          </div>
+          <div>
+            <h2>支出タブ</h2>
+          </div>
+        </div>
+        <div className='RegisterContainer'>
+          <h2>登録エリア</h2>
+        </div>
+      </div>
     </>
   )
 }
